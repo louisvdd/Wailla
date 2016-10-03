996cbbcdc9286b1fa7bd8de9221db53580a08227
AntoniManiscalco <antonimaniscalco@gmail.com>
2016-10-03 09:23:19 +0200

    Oli galère

---
+++ Labo3/Model/Contact.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Contact
+    {
+        public string FirstName
+        {
+            get;
+            set;
+        }
+
+        public string LastName
+        {
+            get;
+            set;
+        }
+
+        public string Email
+        {
+            get;
+            set;
+        }
+
+        public Contact(string f, string l, string e)
+        {
+            FirstName = f;
+            LastName = l;
+            Email = e;
+        }
+    }
+}
